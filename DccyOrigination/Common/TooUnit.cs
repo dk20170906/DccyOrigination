e61ec62831c24cb0d217e9345d6e8e2469ed298a
dk20170906 <605588602@qq.com>
2018-08-28 09:25:20 +0800

    first commit

---
+++ DccyOrigination/Common/TooUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DccyOrigination.Common
+{
+    /// <summary>
+    /// 工具类
+    /// </summary>
+    public class TooUnit
+    {
+     
+    }
+}
---
+++ DccyOrigination/EF/DccyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DccyOrigination.EF
+{
+    public class DccyDbContext:DbContext
+    {
+        public DccyDbContext(DbContextOptions options)
+           : base(options)
+        {
+        }
+
+
+    }
+}
